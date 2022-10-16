9a5af75cdbcb66f2d9ddec8ff114f2cc064a6501
Veronika Savluk <s_veronica@ukr.net>
2022-10-17 01:47:38 +0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import { Route, Routes, NavLink } from 'react-router-dom';
+import Home from 'pages/home/Home';
+import Movies from 'pages/movies/Movies';
+import MovieDetails from 'pages/moviedetails/MovieDetails';
+import Cast from './Cast';
+import Reviews from './Reviews';
+
+const navItems = [
+  { href: '/', text: 'Home' },
+  { href: 'movies', text: 'Movies' },
+];
+
+export const App = () => {
+  return (
+    <>
+    <header>
+      <nav>
+        {navItems.map(({ href, text }) => (<NavLink to={href} key={href}>{text}
+        </NavLink>))}
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+           <Route path="reviews" element={<Reviews />} />
+        </Route>
+      <Route path="*" element={<Home/>} />
+    </Routes>
+    </>
+  );
+};
