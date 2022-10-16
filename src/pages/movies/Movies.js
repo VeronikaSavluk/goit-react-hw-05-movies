6a8af6e8b5cadf20134dcc007d3dfed3277bbea4
Veronika Savluk <s_veronica@ukr.net>
2022-10-17 01:46:01 +0300

    Create Movies.js

---
+++ src/pages/movies/Movies.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { fetchSearchMovies } from '../../components/api';
+
+function Movies() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    const [movies, setMovies] = useState([]);
+    const query = searchParams.get("query") ?? "";
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const { elements: { search }, } = e.currentTarget;
+    const searchQuery = search.value.trim().toLowerCase();
+    if (!searchQuery) {
+        return;
+        };
+    return setSearchParams(searchQuery !== "" ? {query: searchQuery} : {});
+    };
+
+    useEffect(() => {
+        if (!query) {
+            return;
+        };
+        async function fetchSearchAPI() {
+            try {
+                const results = await fetchSearchMovies(query);
+                setMovies(results);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchSearchAPI();
+    }, [query]);
+
+    return (
+        <main>
+            <Link to="/">Go back</Link>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="search"
+                    required
+                    autoFocus
+                />
+                <button type="submit" />
+            </form>
+            <div>
+                {movies && movies.map(({ id, title }) => (
+                    <Link key={id} to={`${id}`} state={{ from: location }}>
+                        {title}
+                    </Link>))}
+            </div>
+        </main>
+    );
+};
+
+export default Movies;
