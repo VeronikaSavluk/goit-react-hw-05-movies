7bede4117fdc9c4516e201933282a1bce16dc330
Veronika Savluk <s_veronica@ukr.net>
2022-10-19 02:05:03 +0300

    Create MovieDetails.styled.js

---
+++ src/pages/moviedetails/MovieDetails.styled.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { theme } from '../../styles';
+
+export const Container = styled.main`
+padding: 20px;
+`;
+
+export const MovieInfo = styled.div`
+display: flex;
+align-itms: flex-start;
+margin: 10px 0;
+`;
+
+export const Poster = styled.img`
+margin-right: 15px;
+`;
+export const Name = styled.h2`
+margin-bottom: 10px;
+`;
+
+export const Title = styled.h3`
+margin-bottom: 10px;
+`;
+
+export const Text = styled.p`
+margin-bottom: 10px;
+text-align: justify;
+`;
+
+export const InfoList = styled.ul`
+
+`;
+
+export const InfoItem = styled.li`
+font-weight: 500;
+font-size: ${theme.fontSizes.medium};
+color: ${theme.colors.blue};
+&:not(:last-child){
+margin-bottom: 8px;
+}
+`;
