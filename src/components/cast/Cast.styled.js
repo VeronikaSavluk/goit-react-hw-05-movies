0c1fcf4e6fd07b1cdd55dbb2cb0c5dc6ea47736f
Veronika Savluk <s_veronica@ukr.net>
2022-10-19 02:03:59 +0300

    Create Cast.styled.js

---
+++ src/components/cast/Cast.styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { theme } from '../../styles';
+
+export const CastList = styled.ul`
+padding: 20px 0;
+`;
+
+export const CastItem = styled.li`
+margin-bottom: 20px;
+`;
+
+export const Portret = styled.img`
+width: 45px;
+height: 65px;
+`;
+
+export const CastTitle = styled.p`
+margin: 8px 0;
+font-size: ${theme.fontSizes.medium};
+font-weight: 500;
+`;
+
+export const CastContent = styled.p`
+text-align: justify;
+font-size: ${theme.fontSizes.small};
+font-weight: 500;
+`;
+
+export const Character = styled.span`
+font-weight: normal;
+`;
