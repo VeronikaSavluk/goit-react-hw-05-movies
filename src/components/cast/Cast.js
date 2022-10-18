157d65e098a81506e956557e285d83884abbffc3
Veronika Savluk <s_veronica@ukr.net>
2022-10-19 02:09:52 +0300

    Create Cast.js

---
+++ src/components/cast/Cast.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieCast } from '../api';
+import defaultPhoto from '../../images/nophoto.jpg';
+import { CastList, CastItem, Portret, CastTitle, CastContent, Character } from './Cast.styled';
+
+const Cast = () => {
+    const [cast, setCast] = useState([]);
+    const {movieId} = useParams();
+
+    useEffect(() => {
+        const getFetchCast = async () => {
+            try {
+                const newCast = await fetchMovieCast(Number(movieId));
+                return setCast(newCast.length > 0 ? newCast : []);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getFetchCast();
+    }, [movieId]);
+
+    return (
+        <CastList>
+            {cast && cast.map(({ id, name, original_name, profile_path, character }) => {
+                const path = profile_path ? `https://image.tmdb.org/t/p/w45/${profile_path}` : defaultPhoto;
+                return (
+                    <CastItem key={id}>
+                        <Portret src={path} alt={name} />
+                        <CastTitle>{original_name}</CastTitle>
+                        <CastContent>Character: <Character>{character}</Character></CastContent>
+                    </CastItem>
+                );
+            })}
+        </CastList>
+    );
+};
+
+export default Cast;
