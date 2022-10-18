2eb95d2b754875d84a230d7d3f519db9adb245bd
Veronika Savluk <s_veronica@ukr.net>
2022-10-19 02:04:12 +0300

    Create Reviews.styled.js

---
+++ src/components/reviews/Reviews.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ReviewsList = styled.ul`
+padding: 20px 0;
+`;
+
+export const ReviewsItem = styled.li`
+margin-bottom: 20px;
+`;
+
+export const ReviewsTitle = styled.h4`
+margin-bottom: 10px;
+`;
+
+export const ReviewsContent = styled.p`
+text-align: justify;
+`;
