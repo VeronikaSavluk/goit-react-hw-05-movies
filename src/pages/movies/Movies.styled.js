e2d26a97e08ebb83ee536028e5f9d0f15981cadf
Veronika Savluk <s_veronica@ukr.net>
2022-10-19 22:33:15 +0300

    Update Movies.styled.js

--- src/pages/movies/Movies.styled.js
+++ src/pages/movies/Movies.styled.js
@@ -1,23 +1,39 @@
 import styled from "styled-components";
 import { theme } from '../../styles';
+import { GoSearch } from 'react-icons/go';
 
 export const SearchForm = styled.form`
 margin: 10px 0;
 `;
 
 export const SearchInput = styled.input`
-border: 1px solid ${theme.colors.grey};
+font-size: ${theme.fontSizes.large};
+padding-left: 15px;
+text-aling: center;
+height: 35px;
+border: 2px solid ${theme.colors.green};
 border-right-style: hidden;
+background: inherit;
+box-shadow: ${theme.shadows.small};
+border-top-left-radius: 15px;
+border-bottom-left-radius: 15px;
 &:focus {
     outline: none;
 }
 `;
 
 export const SearchBtn = styled.button`
-width: 45px;
-height: 23px;
+width: 60px;
+height: 35px;
 text-align: center;
-border: 1px solid ${theme.colors.grey};
+border: 2px solid ${theme.colors.green};
+box-shadow: ${theme.shadows.small};
+border-top-right-radius: 15px;
+border-bottom-right-radius: 15px;
+`;
+
+export const SearchSvg = styled(GoSearch)`
+rotate: 90deg;
 `;
 
 export const SearchItem = styled.li`
