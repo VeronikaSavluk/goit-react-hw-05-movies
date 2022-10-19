a4728e35cea6bf00f72b10f6799757266518ae6a
Veronika Savluk <s_veronica@ukr.net>
2022-10-20 02:12:00 +0300

    Update Layout.styled.js

--- src/components/layout/Layout.styled.js
+++ src/components/layout/Layout.styled.js
@@ -3,14 +3,17 @@ import { NavLink } from 'react-router-dom';
 import { theme } from 'styles';
 
 export const Header = styled.header`
+    position: sticky;
+    top: 0;
     padding: 20px;
     height: 60px;
     border-bottom: 1px solid #A9A9A9;
     box-shadow: 0 5px 7px -1px rgba(51, 51, 51, 0.23);
+    background-color: ${theme.colors.green};
 `;
 
 export const Link = styled(NavLink)`
-color: inherit;
+color: ${theme.colors.lightgreen};
 font-weight: 700;
 text-decoration: none;
 &.active {
