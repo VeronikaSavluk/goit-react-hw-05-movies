68fd2d14319bc4eb4cbb402ed1bf51c2341653ff
Veronika Savluk <s_veronica@ukr.net>
2022-10-19 04:04:52 +0300

    added styles

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,8 @@
 import { Link, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchTraidingMovies } from 'components/api';
+import { Title, MoviesItem } from './Home.styled';
+import { Container } from '../moviedetails/MovieDetails.styled';
 
 function Home() {
     const [movies, setMovies] = useState([]);
@@ -20,16 +22,14 @@ function Home() {
     }, []);
 
     return (
-        <div>
-            <div as="main">
-                <h1>Traiding today</h1>
-                {movies && movies.map(({ id, title }) => (
-                    <Link key={id} to={`movies/${id}`} state={{ from: location }}>
-                        {title}
-                    </Link>))}
-                {error && <p>{error}</p>}
-            </div>
-        </div>
+        <Container>
+            <Title>Traiding today</Title>
+            {movies && <ul>{movies.map(({ id, title }) => (
+                <MoviesItem><Link key={id} to={`movies/${id}`} state={{ from: location }}>
+                {title}</Link></MoviesItem>))}
+            </ul>}
+            {error && <p>{error}</p>}
+        </Container>
 
     );
 };
